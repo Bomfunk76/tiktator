92791d48647f8f7971a8a7850f90da6d1f76ef6d
Taras Stadnik <n0tail300905@gmail.com>
2021-03-27 23:26:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+  // let burgerMenu = document.querySelectorAll(".burger-menu"),
+  //     popupWrap = document.querySelector(".popup-wrap");
+  //     mainMenu = document.querySelector(".main-menu--wrap:not(.main-page-menu)");
+  
+  // if (burgerMenu) { 
+  //   burgerMenu.forEach(el => {
+  //     el.addEventListener("click", function () {
+  //       mainMenu.classList.toggle("active");
+  //       document.querySelector(".main-menu").classList.toggle("active");
+  //     });
+  //   });
+  // }
+  // if (popupWrap) {
+  //   popupWrap.addEventListener("click", function () {
+  //     mainMenu.classList.remove("active");
+  //     document.querySelector(".main-menu").classList.remove("active");
+  //   });
+  // }
+
+  // let mainPageBtn = document.querySelector(".main-page--btn"),
+  //     wpcf7 = document.querySelector(".wpcf7");
+  
+  // if (mainPageBtn) {
+  //   mainPageBtn.addEventListener("click", function () {
+  //     if (window.matchMedia("(min-width: 992px)").matches) {
+  //       document.querySelector(".main-page--container").classList.add("for-popup");
+  //       wpcf7.classList.add("active");
+  //     } else{
+  //       location.href = "/contact-us/";
+  //     }
+  //   });
+
+  //   document.querySelector(".screen-reader-response").addEventListener("click", function () {
+  //     if (window.matchMedia("(min-width: 992px)").matches) {
+  //       document.querySelector(".main-page--container").classList.remove("for-popup");
+  //       wpcf7.classList.remove("active");
+  //     }
+  //   });
+  // }
+});
